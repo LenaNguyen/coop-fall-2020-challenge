994d75a4500598f230990fa582dd77f442d69f73
LenaNguyen27 <nguyen.lena27@hotmail.com>
2020-07-10 14:35:09 -0400

    primary solution

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,14 +1,67 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.historyStack = [];
+    this.futureStack = [];
   }
 
-  add(num) {}
-  subtract(num) {}
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+  add(num) {
+    this.value += num;
+    this.historyStack.push(num);
+    return this.value;
+  }
+
+  subtract(num) {
+    this.value -= num;
+    this.historyStack.push(num * -1);
+    return this.value;
+  }
+
+  undo() {
+    if (!this.historyStack.length) {
+      return this.value;
+    }
+
+    const operation = this.historyStack.pop();
+    this.futureStack.push(operation);
+    this.value -= operation;
+
+    return this.value;
+  }
+
+  redo() {
+    if (!this.futureStack.length) {
+      return this.value;
+    }
+
+    const operation = this.futureStack.pop();
+    this.historyStack.push(operation);
+    this.value += operation;
+
+    return this.value;
+  }
+
+  bulk_undo(num) {
+    let numOperations =
+      num > this.historyStack.length ? this.historyStack.length : num;
+
+    for (let i = 0; i < numOperations; i++) {
+      this.undo();
+    }
+
+    return this.value;
+  }
+
+  bulk_redo(num) {
+    let numOperations =
+      num > this.futureStack.length ? this.futureStack.length : num;
+
+    for (let i = 0; i < numOperations; i++) {
+      this.redo();
+    }
+
+    return this.value;
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
